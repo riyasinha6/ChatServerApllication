9dcab179818525e95e34c1da6abd70d15357c6d6
Riya Sinha <145200820+riyasinha6@users.noreply.github.com>
2025-01-29 01:46:38 +0530

    Update ClientEnd.java

--- ClientEnd.java
+++ ClientEnd.java
@@ -26,24 +26,24 @@ public class ClientEnd {
     DataInputStream input;
     DataOutputStream output;
     private JScrollPane scrollPane;
-  
+
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					ClientEnd window = new ClientEnd();
 					window.frame.setVisible(true);
-				
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-		
+
 		con = new Socket("127.0.0.1", 8080);
 		 while (true) {
 			try {
-				
+
 				DataInputStream input = new DataInputStream(con.getInputStream());
 				String string = input.readUTF();
 				textArea.setText(textArea.getText() + "\n" + "Server: " + string);
@@ -57,14 +57,10 @@ public class ClientEnd {
 						e.printStackTrace();
 					}
 			}
-			
-				
-
 		}
-		
+
 	}
 
-	
 	public ClientEnd() {
 		initialize();
 	}
@@ -75,8 +71,8 @@ public class ClientEnd {
 		frame.setBounds(100, 100, 605, 378);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		frame.setTitle("Soumyadip Networking Project : Client Chat");
-		
+		frame.setTitle("Riya Networking Project : Client Chat");
+
 		textField = new JTextField();
 		textField.setFont(new Font("Lato Medium", Font.PLAIN, 22));
 		textField.setForeground(Color.ORANGE);
@@ -84,14 +80,14 @@ public class ClientEnd {
 		textField.setBounds(12, 45, 344, 38);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		JButton btnNewButton = new JButton("Send");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				if (textField.getText().equals("")) {
 					JOptionPane.showMessageDialog(null, "Please write some text !");
-				}else  {
+				} else {
 					textArea.setText(textArea.getText() + "\n" + "Client : " + textField.getText());
 					try {
 						DataOutputStream output = new DataOutputStream(con.getOutputStream());
@@ -108,21 +104,18 @@ public class ClientEnd {
 					}
 					textField.setText("");
 				}
-			
-			
 			}
-					
 		});
 		btnNewButton.setFont(new Font("Georgia", Font.BOLD, 22));
 		btnNewButton.setForeground(Color.WHITE);
 		btnNewButton.setBackground(Color.RED);
 		btnNewButton.setBounds(398, 45, 164, 38);
 		frame.getContentPane().add(btnNewButton);
-		
+
 		scrollPane = new JScrollPane();
 		scrollPane.setBounds(12, 149, 557, 157);
 		frame.getContentPane().add(scrollPane);
-		
+
 		textArea = new JTextArea();
 		scrollPane.setViewportView(textArea);
 		textArea.setFont(new Font("Comic Sans MS", Font.PLAIN, 22));
